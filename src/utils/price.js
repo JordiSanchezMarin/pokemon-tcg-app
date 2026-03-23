edaf99a3e6df2241257fac672af58ef9dbe059c2
Kalush <jordisanchez00@gmail.com>
2026-03-23 18:31:01 +0100

    feat: new set neo discovery english

--- src/utils/price.js
+++ src/utils/price.js
@@ -8,6 +8,7 @@ import ascendedHeroesEsDb from '../bdd/cards_ascended_heroes_db-es.json';
 import ascendedHeroesEnDb from '../bdd/cards_ascended_heroes_db-en.json';
 import phantasmalFlamesEsDb from '../bdd/cards_phantasmal_flames_db-es.json';
 import phantasmalFlamesEnDb from '../bdd/cards_phantasmal_flames_db-en.json';
+import neoDiscoveryEnDb from '../bdd/cards_neo_discovery_db-en.json';
 
 export const LANG_NAMES = {
   en: 'Inglés',
@@ -23,6 +24,7 @@ const DATABASES = {
   fossil: { none: fossilDb },
   team_rocket: { none: teamRocketDb },
   neo_genesis: { es: neoGenesisEsDb, en: neoGenesisEnDb },
+  neo_discovery: { none: neoDiscoveryEnDb },
   ascended_heroes: { en: ascendedHeroesEnDb, es: ascendedHeroesEsDb },
   phantasmal_flames: { es: phantasmalFlamesEsDb, en: phantasmalFlamesEnDb }
 };
@@ -50,6 +52,7 @@ function normalizeEdicion(card) {
   else if (edicion === 'base4') edicion = 'base2';
   else if (edicion === 'base5') edicion = 'team_rocket';
   else if (edicion === 'neo1') edicion = 'neo_genesis';
+  else if (edicion === 'neo2') edicion = 'neo_discovery';
   else if (edicion === 'me02.5') edicion = 'ascended_heroes';
   else if (edicion === 'me02') edicion = 'phantasmal_flames';
   return edicion;
